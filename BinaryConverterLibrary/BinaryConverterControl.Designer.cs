d53349d59678d9860794a970184d50ef99c19689
Victoria Konareva <v.v.konarieva@student.khai.edu>
2025-05-21 12:44:19 +0300

    Добавьте файлы проекта.

---
+++ BinaryConverterLibrary/BinaryConverterControl.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace BinaryConverterLibrary
+{
+    partial class BinaryConverterControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        // Оголошення елементів керування
+        private System.Windows.Forms.TextBox textBoxDecimal;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox textBoxBinary;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true, якщо керовані ресурси слід видалити; інакше, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Метод, необхідний для підтримки конструктора — не змінюйте
+        /// вміст цього методу за допомогою редактора коду.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxDecimal = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.textBoxBinary = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxDecimal
+            // 
+            this.textBoxDecimal.Location = new System.Drawing.Point(10, 10);
+            this.textBoxDecimal.Name = "textBoxDecimal";
+            this.textBoxDecimal.Size = new System.Drawing.Size(120, 20);
+            // Встановлення Anchor: верх, ліво, право
+            this.textBoxDecimal.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDecimal.TabIndex = 0;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(10, 40);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(120, 25);
+            // Встановлення Anchor: верх, ліво, право
+            this.btnConvert.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "Перетворити";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // textBoxBinary
+            // 
+            this.textBoxBinary.Location = new System.Drawing.Point(10, 80);
+            this.textBoxBinary.Name = "textBoxBinary";
+            this.textBoxBinary.ReadOnly = true;
+            this.textBoxBinary.Size = new System.Drawing.Size(120, 20);
+            // Встановлення Anchor: верх, ліво, право
+            this.textBoxBinary.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxBinary.TabIndex = 2;
+            // 
+            // BinaryConverterControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textBoxBinary);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.textBoxDecimal);
+            this.Name = "BinaryConverterControl";
+            this.Size = new System.Drawing.Size(150, 120);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+    }
+}
---
+++ BinaryConverterLibrary/BinaryConverterControl.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BinaryConverterLibrary
+{
+    [ToolboxBitmap(typeof(BinaryConverterControl), "BinaryConverterControl.ico")]
+    public partial class BinaryConverterControl : UserControl
+    {
+        public BinaryConverterControl()
+        {
+            InitializeComponent(); // Виклик створеного дизайнера
+            // Додатковий код ініціалізації (якщо потрібно)
+        }
+
+        // Властивість для роботи з десятковим числом
+        public int DecimalValue
+        {
+            get => int.TryParse(textBoxDecimal.Text, out int value) ? value : 0;
+            set
+            {
+                textBoxDecimal.Text = value.ToString();
+                UpdateBinaryValue();
+            }
+        }
+
+        // Властивість для роботи з двійковим рядком
+        public string BinaryValue
+        {
+            get => textBoxBinary.Text;
+            private set => textBoxBinary.Text = value;
+        }
+
+        // Обробка натискання кнопки
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            UpdateBinaryValue();
+            OnConverted(EventArgs.Empty);
+        }
+
+        // Метод конвертації десяткового числа у двійковий рядок
+        private void UpdateBinaryValue()
+        {
+            if (int.TryParse(textBoxDecimal.Text, out int value))
+            {
+                BinaryValue = Convert.ToString(value, 2);
+            }
+            else
+            {
+                BinaryValue = "Некоректне число";
+            }
+        }
+
+        // Подія, що сповіщає про завершення конвертації
+        public event EventHandler Converted;
+        protected virtual void OnConverted(EventArgs e)
+        {
+            Converted?.Invoke(this, e);
+        }
+    }
+}
